3fff719eb2371da157435c45b23bbe75b04af904
Spencer Ye <sye348@student.fuhsd.org>
2020-11-27 20:12:57 -0800

    Update names

--- bot.py
+++ bot.py
@@ -63,8 +63,9 @@ async def on_message(ctx, who, times=1):
 	log("CMD: fatshame")
 	if who == "":
 		await ctx.send("Please Specify Who Is Fat [Example: yefatshame @ye boi]")
-	for i in range(0, 1):
-		await ctx.send(__options.fatgenerate(str(who)) * times)
+	if times > 20:
+		await ctx.send("Fat shame them a little less would ya?")
+	await ctx.send(__options.fatgenerate(str(who)) * times)
 
 @bot.command(name="1v1", help="[n/a] sends a 1v1")
 async def on_message(ctx, attendee):
@@ -79,7 +80,7 @@ async def on_message(ctx):
 	log("CMD: k")
 	await ctx.send("read the help menu and piss off")
 
-@bot.command(name="setscore", help="sets a new score for count")
+@bot.command(name="logscore", help="sets a new score for count")
 async def on_message(ctx, arg):
 	log("CMD: setscore")
 	print(str(ctx.guild))
@@ -88,7 +89,7 @@ async def on_message(ctx, arg):
 	file.close()
 	await ctx.send("New Score: " + str(arg) + " Logged")
 
-@bot.command(name="topscore", help="Gets the top count score for this server")
+@bot.command(name="topscores", help="Gets the top count score for this server")
 async def on_message(ctx):
 	log("CMD: setscore")
 	print(str(ctx.guild))
