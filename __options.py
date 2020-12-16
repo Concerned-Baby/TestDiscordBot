7d92441302560d2ad4085ac19273162efcd101c2
Spencer Ye <sye348@student.fuhsd.org>
2020-12-15 20:46:28 -0800

    update options

--- __options.py
+++ __options.py
@@ -13,7 +13,7 @@ fatquotes = [
 	namereplace + " is so fat, McDonalds won't serve them\n",
 	namereplace + " is so fat, he's " + namereplace + "\n",
 	namereplace + " is round\n",
-	namereplace + " doesn't run\n"
+	namereplace + " doesn't run at all\n"
 ]
 
 def fatgenerate(name):
