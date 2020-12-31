ab05fac889b9843b826ab7a898c4d341cf37aeef
Spencer Ye <sye348@student.fuhsd.org>
2020-12-30 22:37:53 -0800

    Update __options.py

--- __options.py
+++ __options.py
@@ -4,17 +4,18 @@ global fatquotes
 global namereplace
 namereplace = "*[77687920776f756c64207520646f2074686973]*"
 fatquotes = [
-	namereplace + " is fat lmao\n",
-	namereplace + " has a double chin lmao\n",
-	namereplace + " is like my mom\n",
-	namereplace + " is way too thicc\n",
-	namereplace + " needs to workout more\n",
-	namereplace + " can only roll down hills\n",
-	namereplace + " is so fat, McDonalds won't serve them\n",
-	namereplace + " is so fat, he's %s\n" % namereplace,
-	namereplace + " is round\n",
-	namereplace + " does not lift\n",
+	"%s is fat lmao\n" % namereplace,
+	"%s has a double chin lmao\n" % namereplace,
+	"%s is like my mom\n" % namereplace,
+	"%s is way too thicc\n" % namereplace,
+	"%s needs to workout more\n" % namereplace,
+	"%s can only roll down hills\n" % namereplace,
+	"%s is so fat, McDonalds won't serve them\n" % namereplace,
+	"%s is so fat, he's %s\n" % namereplace,
+	"%s is round\n" % namereplace,
+	"%s does not lift\n" % namereplace,
 	"i just hate %s because he's fat\n" % namereplace
+
 ]
 
 def fatgenerate(name):
